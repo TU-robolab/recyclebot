0f01d0d29ed53d011946b25b8b6c2acfafe72843
Ryudas <sivlesegrob9@gmail.com>
2025-02-20 11:34:04 +0100

    update for wayland compability

--- export_env.sh
+++ export_env.sh
@@ -7,3 +7,5 @@ echo "USER_ID=$(id -u $USER)" >> ${ENV_FILE}
 echo "GROUP_NAME=$(id -gn $USER)" >> ${ENV_FILE}
 echo "GROUP_ID=$(id -g $USER)" >> ${ENV_FILE}
 echo "DISPLAY=${DISPLAY}" >> ${ENV_FILE}
+echo "WAYLAND_DISPLAY=${WAYLAND_DISPLAY}" >> ${ENV_FILE}
+echo "XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}" >> ${ENV_FILE}
