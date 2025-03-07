676531da6cdbd88de43842872db73c156da0337a
Ryudas <sivlesegrob9@gmail.com>
2025-03-07 08:03:30 +0100

    update sourcing of bash

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
-. "/opt/ros/${ROS_DISTRO}/setup.bash"
-. "${ROS2_WS}/install/setup.bash"
+# Source global ROS setup
+source "/opt/ros/${ROS_DISTRO}/setup.bash"
+
+# Source ROS2 workspace if it exists
+if [ -f "${ROS2_WS}/install/setup.bash" ]; then
+    source "${ROS2_WS}/install/setup.bash"
+fi
+
+# Ensure .bashrc is sourced for interactive shells
+if [[ $- == *i* ]]; then
+    source ~/.bashrc
+fi
 
 exec "$@"
