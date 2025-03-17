6de377ef18a0cdcd06f79becc48df825de296c84
Ryudas <sivlesegrob9@gmail.com>
2025-03-17 11:14:08 +0100

    update setup configuration in docker compose

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 . "/opt/ros/${ROS_DISTRO}/setup.bash"
 . "${ROS2_WS}/install/setup.bash"
 
+# Activate Python virtual environment (if it exists)
+if [ -d "/venv" ]; then
+    source /venv/bin/activate
+fi
+
 exec "$@"
