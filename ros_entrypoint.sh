3d5c20fa686b70494af12558274d8f30a35c0c17
Ryudas <sivlesegrob9@gmail.com>
2025-03-17 15:10:02 +0100

    update the ros entrypoint script

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,13 +3,12 @@ set -e
 
 . "/opt/ros/${ROS_DISTRO}/setup.bash"
 . "${ROS2_WS}/install/setup.bash"
-. "alias ros='ros2'"
+
 
 # activate Python virtual environment (if it exists)
 if [ -d "/venv" ]; then
     source /venv/bin/activate
 fi
 
-#source ~/.bashrc  # alias ros='ros2'
 
 exec "$@"
