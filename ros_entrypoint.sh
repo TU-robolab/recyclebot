ba8bb2404e84ab3f0a12e87d7735d4b352e453c7
Ryudas <sivlesegrob9@gmail.com>
2025-03-17 14:57:34 +0100

    sd

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 . "/opt/ros/${ROS_DISTRO}/setup.bash"
 . "${ROS2_WS}/install/setup.bash"
+. "alias ros='ros2'"
 
 # activate Python virtual environment (if it exists)
 if [ -d "/venv" ]; then
     source /venv/bin/activate
 fi
 
-. "alias ros='ros2'"
 #source ~/.bashrc  # alias ros='ros2'
 
 exec "$@"
