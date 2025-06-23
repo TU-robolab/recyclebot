6cea0b9f99a9a05e6d70fd4e2acdb3d32b5b50a6
Ryudas <sivlesegrob9@gmail.com>
2025-06-23 23:29:13 +0200

    setup share config file for recyclebot

--- packages/recycle_bot/setup.py
+++ packages/recycle_bot/setup.py
@@ -23,10 +23,12 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('**/*.yaml', recursive=True)), # install all yaml files in share
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=install_requires,  # add requirements from requirements.txt
