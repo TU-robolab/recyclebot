1f0b9fffee374209fa6ceeca7d4a34c9b1f1afe7
Ryudas <sivlesegrob9@gmail.com>
2025-06-23 22:54:39 +0200

    edited smoke file and it's interaction

--- packages/recycle_bot/recycle_bot/rec_bot_smoke.py
+++ packages/recycle_bot/recycle_bot/rec_bot_smoke.py
@@ -9,7 +9,7 @@ moveit = MoveItPy(node_name="ur16e_move_client")
 # obtain a PlanningComponent for the manipulator group
 arm = moveit.get_planning_component("ur_manipulator")
 
-# plan to a joint target
+# plan to a joint targets
 goal = [0.0, -1.4, 1.4, -1.4, -1.57, 0.0]
 plan_result = arm.plan_to_joint_positions(goal)
 if plan_result:
