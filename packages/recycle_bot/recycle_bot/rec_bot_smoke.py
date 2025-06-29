179f173b3cbc927d209da54dacf2b2354b48f5d7
Ryudas <sivlesegrob9@gmail.com>
2025-06-29 21:06:41 +0200

    update moveit smoke

--- packages/recycle_bot/recycle_bot/rec_bot_smoke.py
+++ packages/recycle_bot/recycle_bot/rec_bot_smoke.py
@@ -10,7 +10,16 @@ moveit = MoveItPy(node_name="ur16e_move_client")
 arm = moveit.get_planning_component("ur_arm")
 
 # plan to a joint targets
-goal = [0.0, -1.4, 1.4, -1.4, -1.57, 0.0]
-plan_result = arm.plan_to_joint_positions(goal)
+goal = {
+    "shoulder_pan_joint": 0.0,
+    "shoulder_lift_joint": -1.4,
+    "elbow_joint": 1.4,
+    "wrist_1_joint": -1.4,
+    "wrist_2_joint": -1.57,
+    "wrist_3_joint": 0.0,
+}
+
+plan_result = arm.plan(goal)           # ⬅️  was plan_to_joint_positions()
+   # execute the returned plan
 if plan_result:
-    arm.execute()     
+    arm.execute(plan_result.trajectory)   
